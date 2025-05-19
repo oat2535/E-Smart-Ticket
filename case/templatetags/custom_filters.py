7e5a72ab84611a2eb91d105f4b8f12eb5c55a96c
oat2535 <oat_2535@outlook.co.th>
2025-05-19 20:58:51 +0700

    Update : 190525, 20.58

--- case/templatetags/custom_filters.py
+++ case/templatetags/custom_filters.py
@@ -3,8 +3,18 @@ from django import template
 
 register = template.Library()
 
+#ข้อความยาวเกิน 50 ตัวอักษรจะถูกขึ้นบรรทัดใหม่
+# @register.filter
+# def wrap_50(value):
+#     if not isinstance(value, str):
+#         return value
+#     return '<br>'.join([value[i:i+50] for i in range(0, len(value), 50)])
+
+#ตัดข้อความที่ยาวเกิน 50 ตัวอักษร
 @register.filter
 def wrap_50(value):
     if not isinstance(value, str):
         return value
-    return '<br>'.join([value[i:i+50] for i in range(0, len(value), 50)])
+    if len(value) > 50:
+        return value[:50] + '...'
+    return value
